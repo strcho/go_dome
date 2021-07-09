eadfe73f9d8ccf0d269c7f89a4b364edc404ea08
orcs <orcs.xyz@qq.com>
2021-07-09 17:22:21 +0800

    ok

--- start.go
+++ start.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main(){
+// Prefix 为拥有一个或多个单词的链马尔可夫链的前缀。
+type Prefix []string
+
+// String returns the Prefix as a string (for use as a map key).
+
+// String 将 Prefix 作为一个（用作映射键的）字符串返回。
+func (p Prefix) String() string {
+	return strings.Join(p, " ？")
+}
+
+func (p Prefix) Print() string {
+	fmt.Println(p)
+	return "ok!"
+}
+
+func main() {
+	// p := make(Prefix, 4, 5)
+	p := Prefix{"i", "love", "you"}
+	fmt.Println(len(p), cap(p))
 	fmt.Println("hello!")
+	fmt.Println(p.String())
+	p.Print()
 }
